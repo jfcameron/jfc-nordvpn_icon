aebea8858842ff8081a8d29602eb2fd88a4f9a08
Joseph Cameron <jf.cameron@gmail.com>
2021-02-19 19:42:39 -0500

    added additional nordvpn functions
    support for:
    * countries
    * connect
    * disconnect

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,9 @@
 #include <memory>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtk/gtk.h>
-
-static auto UPDATE_RATE_SECONDS(5);
 
 /// \brief runs a shell command, returns the standard output 
 /// as a stringstream
@@ -42,7 +41,7 @@ using nordvpn_status_type = std::unordered_map</*name*/std::string,
 /// \brief returns the nordvpn program status as a map
 const nordvpn_status_type get_nordvpn_status()
 {
-    auto output = run_command("nordvpn status");
+    auto output(run_command("nordvpn status"));
 
     nordvpn_status_type state;
 
@@ -50,7 +49,7 @@ const nordvpn_status_type get_nordvpn_status()
     {
         static const std::string DELIMITER(": ");
 
-        if (auto search = line.find(DELIMITER); 
+        if (auto search(line.find(DELIMITER)); 
             search != std::string::npos && 
             search < line.size() - DELIMITER.size())
         {
@@ -62,13 +61,65 @@ const nordvpn_status_type get_nordvpn_status()
     return state;
 }
 
+/// \brief type used to model the return of nordvpn countries command
+using nordvpn_countries_type = std::vector<std::string>;
+
+/// \brief returns the nordvpn countries as a list
+const nordvpn_countries_type get_nordvpn_countries()
+{
+    auto output(run_command("nordvpn countries"));
+
+    nordvpn_countries_type countries;
+
+    for (std::string line; std::getline(output, line);)
+    {
+        std::string country;
+
+        for (size_t i(0); i < line.size(); ++i)
+        {
+            const auto &c = line[i];
+
+            if (c == '\t' || i >=  line.size() - 1)
+            {
+                if (country.size()) countries.push_back(country);
+
+                country.clear();
+            }
+            else country += c;
+        }
+
+        countries.back().push_back(line.back());
+    }
+
+    return countries;
+}
+
+void nordvpn_connect(const std::string &aCountry, const std::string &aCity)
+{
+    run_command("nordvpn connect " + aCountry + " " + aCity);
+}
+
+void nordvpn_connect(const std::string &aCountry = "")
+{
+    run_command("nordvpn connect " + aCountry);
+}
+
+void nordvpn_disconnect()
+{
+    run_command("nordvpn disconnect");
+}
+
 static bool update()
 {
-    auto status = get_nordvpn_status();
-
-    for (const auto &[name, value] : status)
     {
-        std::cout << name << ", " << value << "\n";
+        auto status = get_nordvpn_status();
+
+        for (const auto &[name, value] : status)
+        {
+            std::cout << name << ", " << value << "\n";
+        }
+
+        std::cout << "\n";
     }
 
     return true;
@@ -76,6 +127,14 @@ static bool update()
 
 int main(int argc, char *argv[])
 {
+    {
+        const auto countries = get_nordvpn_countries();
+        
+        for (auto &a : countries) std::cout << a << "\n";
+    }
+
+    std::cout << "===============\n";
+
     gtk_init(&argc, &argv);
 
     g_timeout_add_seconds(1, [](void *const vp) 
@@ -83,6 +142,8 @@ int main(int argc, char *argv[])
             auto token = reinterpret_cast<std::string *>(vp);
 
             update();
+
+            static auto UPDATE_RATE_SECONDS(5);
 
             g_timeout_add_seconds(UPDATE_RATE_SECONDS, 
                 reinterpret_cast<GSourceFunc>(update), nullptr);
